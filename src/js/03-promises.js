5315dee9ed86d92d62c32e3f7992e04f08a25ede
Vladislav <102545184+lezver@users.noreply.github.com>
2023-04-17 11:56:14 +0300

    third exercise - done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
-function createPromise(position, delay) {
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+const [delay, step, amount, button] = form;
+
+const createPromise = (delay, step, amount) => {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+
+  for (let i = 0; i < amount; i += 1) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldResolve) {
+          resolve(`✅ Fulfilled promise ${1 + i} in ${delay + step * i}ms`);
+        } else {
+          reject(`❌ Rejected promise ${1 + i} in ${delay + step * i}ms`);
+        }
+      }, delay + step * i);
+    });
+
+    promise.then(result => {
+      Notiflix.Notify.success(result);
+    });
+    promise.catch(error => {
+      Notiflix.Notify.failure(error);
+    });
   }
-}
+};
+
+const checkOfFomr = e => {
+  e.preventDefault();
+
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  createPromise(delayValue, stepValue, amountValue);
+
+  form.reset();
+};
+
+form.addEventListener('submit', checkOfFomr);
